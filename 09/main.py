d401d79416b2dd29d5ff339e6fe5c6a9831d2e2a
Eugenio Gimenez <e.o.gimenez@gmail.com>
2023-04-25 20:33:58 +0200

    text parser 1.1.2

--- 09/main.py
+++ 09/main.py
@@ -21,30 +21,31 @@ ruta = Path(
     "/Users/Eu/Documents/Eu/Visual-Studio/EjerciciosPython/09/Mi_Gran_Directorio")
 
 
-def serial_number_search():
+def serial_number_search(ruta):
     dictionary = {}
     for dirs, subdirs, files in os.walk(ruta):
         for file in files:
             search = Path(dirs, file)
             text = search.read_text("utf-8")
-            serial_number = re.search(r"[N]\D{3}-\d{5}", text)
+            serial_number = re.search(r"N\D{3}-\d{5}", text)
             if serial_number:
-                dictionary[f"{file}"] = f"{serial_number[0]}"
+                dictionary[f"{file}"] = f"{serial_number.group()}"
             else:
                 pass
     return dictionary
 
 
 start = time.time()
-dictionary_result = serial_number_search()
+dictionary_result = serial_number_search(ruta)
 end = time.time()
 result = end - start
 
 clear()
-print("-"*25)
-print("ARCHIVOS\tNRO.SERIE")
-print("-----------\t----------")
+print("-"*40)
+print(f"Fecha de busqueda:\t{today.strftime('%d/%m/%Y')}\n")
+print("ARCHIVOS\t\tNRO.SERIE")
+print("-----------\t\t----------")
 for key, value in dictionary_result.items():
-    print(f'{key}\t{value}')
+    print(f'{key}\t\t{value}')
 print(f'Numero encontrados: {len(dictionary_result)}')
 print(f'Duración de la búsqueda: {math.ceil(result)}')
